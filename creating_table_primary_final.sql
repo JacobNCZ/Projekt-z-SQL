8d30e5530ee67f607d694569e25c0997d45f369c
JacobNCZ <152058858+JacobNCZ@users.noreply.github.com>
2024-11-24 12:23:10 +0100

    Update creating_table_primary_final.sql
    Úprava scriptu vytvoření tabulky: t_jakub_narozny_project_SQL_primary_final
    1)	Přímé vytvoření tabulky t_jakub_narozny_project_SQL_primary_final bez pomocných tabulek. 
    2)	Rozepsání „selectu“
    3)	Doplnění aliasů tabulek
    4)	Doplnění názvů sloupců ze „selectu“ do GROUP BY

--- creating_table_primary_final.sql
+++ creating_table_primary_final.sql
@@ -1,36 +1,53 @@
-CREATE TABLE czechia_products
-	(SELECT
-		czechia_price.id AS price_id,
-		category_code AS product_code,
-		name AS product_name,
-		round(avg(value), 2) AS product_price,
-		price_value,
-		price_unit,
-		YEAR(date_from) AS price_year
-	FROM czechia_price
-	INNER JOIN czechia_price_category
-		ON czechia_price.category_code = czechia_price_category.code
-	GROUP BY category_code, YEAR(date_from)
-	ORDER BY category_code);
-
-CREATE TABLE czechia_industry 
-	(SELECT 
-		czechia_payroll.id AS payroll_id, 
+CREATE TABLE t_jakub_narozny_project_SQL_primary_final
+SELECT 
+	value_type_code,
+	unit_code,
+	industry_branch_code,
+	industry_name,
+	payroll_value,
+	payroll_year,
+	product_code,
+	product_name,
+	product_price,
+	price_value,
+	price_unit,
+	price_year
+FROM (SELECT  
 		value_type_code,
 		unit_code,
 		industry_branch_code,
 		name AS industry_name,
 		round(avg(value), 2) AS payroll_value,
 		payroll_year
-	FROM czechia_payroll
-	INNER JOIN czechia_payroll_industry_branch 
-		ON czechia_payroll.industry_branch_code = czechia_payroll_industry_branch.code
+	FROM czechia_payroll AS cz_payr
+	INNER JOIN czechia_payroll_industry_branch AS cz_pibc
+		ON cz_payr.industry_branch_code = cz_pibc.code
 	WHERE industry_branch_code IS NOT NULL AND value_type_code <> 316 
-	GROUP BY industry_branch_code, payroll_year);
+	GROUP BY
+		industry_branch_code,
+		payroll_year,
+		value_type_code,
+		unit_code,
+		industry_name) AS cz_i
+INNER JOIN 
+	(SELECT
+		category_code AS product_code,
+		name AS product_name,
+		round(avg(value), 2) AS product_price,
+		price_value,
+		price_unit,
+		YEAR(date_from) AS price_year
+	FROM czechia_price AS cz_pri
+	INNER JOIN czechia_price_category AS cz_pc
+	ON cz_pri.category_code = cz_pc.code
+	GROUP BY
+		product_code,
+		price_year,
+		product_name,
+		price_value,
+		price_unit
+	ORDER BY category_code) AS cz_p
+ON cz_i.payroll_year = cz_p.price_year
+ORDER BY industry_branch_code, payroll_year, product_code;
 
-CREATE TABLE t_jakub_narozny_project_SQL_primary_final 
-	(SELECT * FROM czechia_industry
-	INNER JOIN czechia_products
-		ON czechia_industry.payroll_year = czechia_products.price_year);
-
-DROP TABLE IF EXISTS czechia_products, czechia_industry;
+SELECT * FROM t_jakub_narozny_project_sql_primary_final;
