308f28b472e9fe919733b40b5d65d004f3193173
JacobNCZ <152058858+JacobNCZ@users.noreply.github.com>
2024-11-24 13:26:17 +0100

    Úprava scriptu 5th_task - Update 5th_task.sql
    5th_task:
    1) Vytvoření pomocné tabulky t_jakub_narozny_project_sql_temp_tbl2 – pro výběr dat HDP
    2) Vytvoření tabulky t_jakub_narozny_project_sql_task05 kde můžeme porovnávat sloupce total_gr_GDP s total_gr_payroll a total_gr_price
    3) Smazání pomocné tabulky

--- 5th_task.sql
+++ 5th_task.sql
@@ -1,96 +1,55 @@
+-- Vytvoření pomocné tabulky:
+CREATE TABLE t_jakub_narozny_project_sql_temp_gdp
 SELECT
 	country,	
 	abbreviation,
 	currency_code,
 	`year`,
-	GDP
+	GDP,
+	LAG (GDP, 1)
+		OVER (PARTITION BY NULL ORDER BY year) AS last_year_price,
+	(((GDP - (LAG (GDP, 1)
+		OVER (PARTITION BY NULL ORDER BY year)))/(LAG (GDP, 1)
+		OVER (PARTITION BY NULL ORDER BY year)))*100) AS total_gr_GDP
 FROM t_jakub_narozny_project_sql_secondary_final
 WHERE country = 'Czech Republic'
 ORDER BY `year`;
 
-INSERT INTO total_payroll_growth_rate (`section`)
-VALUES ('GDP');
+-- Úprava typu sloupce - změna počtu des. míst:
+ALTER TABLE t_jakub_narozny_project_sql_temp_gdp
+MODIFY COLUMN total_gr_GDP DECIMAL(10,2);
 
-UPDATE total_payroll_growth_rate
-	SET `sum(year_2006)` = 
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2006),
-		`sum(year_2007)` =
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2007),
-		`sum(year_2008)` = 
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2008),
-		`sum(year_2009)` =
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2009),
-		`sum(year_2010)` = 
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2010),
-		`sum(year_2011)` = 
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2011),
-		`sum(year_2012)` =
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2012),
-		`sum(year_2013)` =
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2013),
-		`sum(year_2014)` =
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2014),
-		`sum(year_2015)` =
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2015),
-		`sum(year_2016)` = 
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2016),
-		`sum(year_2017)` =
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2017),
-		`sum(year_2018)` =
-		(SELECT GDP 
-		FROM t_jakub_narozny_project_sql_secondary_final
-		WHERE country = 'Czech Republic' AND YEAR = 2018)
-	WHERE `section` = 'GDP';
+-- Vytvoření hlavní tabulky:
+CREATE TABLE t_jakub_narozny_project_sql_task05
+SELECT
+	tab1.country,
+	tab1.abbreviation,
+	tab1.currency_code,
+	tab1.GDP,
+	tab1.total_gr_GDP,
+	tab2.total_gr_payroll,
+	tab2.total_gr_price,
+	tab1.`year`,
+	tab2.total_payroll,
+	tab2.total_price,	
+	tab2.difference
+FROM t_jakub_narozny_project_sql_temp_gdp AS tab1
+INNER JOIN (SELECT
+	t1.`year`,
+	t1.total_payroll,
+	t1.total_gr_payroll,
+	total_price,
+	total_gr_price,
+	diff_tab.difference
+FROM t_jakub_narozny_project_sql_task04 AS t1
+INNER JOIN (SELECT `year`, (total_gr_price - total_gr_payroll) AS difference
+FROM t_jakub_narozny_project_sql_task04) AS diff_tab
+ON t1.`year` = diff_tab.YEAR
+ORDER BY difference DESC) AS tab2
+ON tab1.`year` = tab2.`year`;
 
-UPDATE total_payroll_growth_rate
-SET	total_payroll_growth_rate.total_gr_07 = (((`sum(year_2007)` - `sum(year_2006)`)/`sum(year_2006)`)*100),
-	total_payroll_growth_rate.total_gr_08 = (((`sum(year_2008)` - `sum(year_2007)`)/`sum(year_2007)`)*100),
-	total_payroll_growth_rate.total_gr_09 = (((`sum(year_2009)` - `sum(year_2008)`)/`sum(year_2008)`)*100),
-	total_payroll_growth_rate.total_gr_10 = (((`sum(year_2010)` - `sum(year_2009)`)/`sum(year_2009)`)*100),
-	total_payroll_growth_rate.total_gr_11 = (((`sum(year_2011)` - `sum(year_2010)`)/`sum(year_2010)`)*100),
-	total_payroll_growth_rate.total_gr_12 = (((`sum(year_2012)` - `sum(year_2011)`)/`sum(year_2011)`)*100),
-	total_payroll_growth_rate.total_gr_13 = (((`sum(year_2013)` - `sum(year_2012)`)/`sum(year_2012)`)*100),
-	total_payroll_growth_rate.total_gr_14 = (((`sum(year_2014)` - `sum(year_2013)`)/`sum(year_2013)`)*100),
-	total_payroll_growth_rate.total_gr_15 = (((`sum(year_2015)` - `sum(year_2014)`)/`sum(year_2014)`)*100),
-	total_payroll_growth_rate.total_gr_16 = (((`sum(year_2016)` - `sum(year_2015)`)/`sum(year_2015)`)*100),
-	total_payroll_growth_rate.total_gr_17 = (((`sum(year_2017)` - `sum(year_2016)`)/`sum(year_2016)`)*100),
-	total_payroll_growth_rate.total_gr_18 = (((`sum(year_2018)` - `sum(year_2017)`)/`sum(year_2017)`)*100);
+-- Vymazání pomocné tabulky:
+DROP TABLE IF EXISTS t_jakub_narozny_project_sql_temp_gdp;
 
-SELECT 
-	`section`,
-	total_gr_07,
-	total_gr_08,
-	total_gr_09,
-	total_gr_10,
-	total_gr_12,
-	total_gr_13,
-	total_gr_14,
-	total_gr_15,
-	total_gr_16,
-	total_gr_17,
-	total_gr_18
-FROM total_payroll_growth_rate;
+-- Finální výbar požadovaných dat:
+SELECT * FROM t_jakub_narozny_project_sql_task05;
