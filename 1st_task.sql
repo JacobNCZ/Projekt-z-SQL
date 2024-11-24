3346114986aa42d82c459d5afafaecc6e0fc2556
JacobNCZ <152058858+JacobNCZ@users.noreply.github.com>
2024-11-24 12:30:02 +0100

    Update 1st_task.sql
    Úprava scriptu 1st_task
    1)	Vytvoření tabulky t_jakub_narozny_sql_task01
    2)	Doplnění sloupce last_year a difference (rozdílu payroll value za předešlý rok)
    3)	Výběr jednotlivých let s negativní hodnotou tedy – kdy došlo k poklesu

--- 1st_task.sql
+++ 1st_task.sql
@@ -1,7 +1,16 @@
+CREATE TABLE t_jakub_narozny_project_sql_task01
 SELECT
 	industry_branch_code,
 	industry_name,
+	payroll_year,
 	payroll_value,
-	payroll_year
+	LAG (payroll_value, 1)
+		OVER (PARTITION BY industry_branch_code ORDER BY payroll_year) AS last_year,
+	(payroll_value - (LAG (payroll_value, 1)
+		OVER (PARTITION BY industry_branch_code ORDER BY payroll_year))) AS difference 
 FROM t_jakub_narozny_project_sql_primary_final
 GROUP BY industry_branch_code, payroll_year;
+
+SELECT * 
+FROM t_jakub_narozny_project_sql_task01
+WHERE difference < 0;
